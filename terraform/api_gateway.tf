02d870c7666454847e0aacfd73e5163efdee14e5
Victor <me@vic.com>
2022-11-28 02:18:40 -0600

    Tuned lambda and gateway terraform files

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,58 +1,58 @@
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "GoServerlessExample"
-  description = "Terraform Serverless Application Example"
+resource "aws_api_gateway_rest_api" "ipquery" {
+  name        = "IP Query API"
+  description = "API Gateway to execute lambda fn"
 }
 
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.ipquery.id}"
+  parent_id   = "${aws_api_gateway_rest_api.ipquery.root_resource_id}"
   path_part   = "{proxy+}"
 }
 
 resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
+  rest_api_id   = "${aws_api_gateway_rest_api.ipquery.id}"
   resource_id   = "${aws_api_gateway_resource.proxy.id}"
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.ipquery.id}"
   resource_id = "${aws_api_gateway_method.proxy.resource_id}"
   http_method = "${aws_api_gateway_method.proxy.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.example.invoke_arn}"
+  uri                     = "${aws_lambda_function.ipquery.invoke_arn}"
 }
 
 resource "aws_api_gateway_method" "proxy_root" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
+  rest_api_id   = "${aws_api_gateway_rest_api.ipquery.id}"
+  resource_id   = "${aws_api_gateway_rest_api.ipquery.root_resource_id}"
   http_method   = "ANY"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_root" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.ipquery.id}"
   resource_id = "${aws_api_gateway_method.proxy_root.resource_id}"
   http_method = "${aws_api_gateway_method.proxy_root.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.example.invoke_arn}"
+  uri                     = "${aws_lambda_function.ipquery.invoke_arn}"
 }
 
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_deployment" "ipquery" {
   depends_on = [
     "aws_api_gateway_integration.lambda",
     "aws_api_gateway_integration.lambda_root",
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.ipquery.id}"
   stage_name  = "test"
 }
 
 output "base_url" {
-  value = "${aws_api_gateway_deployment.example.invoke_url}"
+  value = "${aws_api_gateway_deployment.ipquery.invoke_url}"
 }
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,26 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# lambda definition
 resource "aws_lambda_function" "ipquery" {
   function_name = "ipQuery"
 
-  # The bucket name as created earlier with "aws s3api create-bucket"
-  s3_bucket = "terraform-serverless-example-dupadipanga"
-  s3_key    = "v1.0.0/example.zip"
+  # I'm using S3 to store the compiled golang project
+  s3_bucket = "terraform-serverless-votazu-ipquery"
+  s3_key    = "v1.0.0/main.zip"
 
-  # "main" is the filename within the zip file (main.js) and "handler"
-  # is the name of the property under which the handler function was
-  # exported in that file.
+  # "main" is the filename within the zip file (main.zip)
   handler = "main"
   runtime = "go1.x"
 
   role = "${aws_iam_role.lambda_exec.arn}"
 }
 
-# IAM role which dictates what other AWS services the Lambda function
-# may access.
+# AWS services the Lambda function can access.
 resource "aws_iam_role" "lambda_exec" {
-  name = "goserverless_example_lambda"
+  name = "goserverless_ipquery_lambda"
 
   assume_role_policy = <<EOF
 {
@@ -40,6 +38,7 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
+# Logs configuration
 data "aws_iam_policy_document" "lambda_exec_role_policy" {
   statement {
     actions = [
@@ -58,13 +57,12 @@ resource "aws_iam_role_policy" "lambda_exec_role" {
   policy = data.aws_iam_policy_document.lambda_exec_role_policy.json
 }
 
+# Gateway permission
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.example.function_name}"
+  function_name = "${aws_lambda_function.ipquery.function_name}"
   principal     = "apigateway.amazonaws.com"
 
-  # The /*/* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
-  source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.ipquery.execution_arn}/*/*"
 }
